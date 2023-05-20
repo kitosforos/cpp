5f211d63c321902362a40ddda191d25c0c81ea00
kitosforos <kitosforos@gmail.com>
2023-05-20 21:43:20 +0200

    implementado el main y weapon

---
+++ cpp01/Weapon.cpp
@@ -0,0 +1,24 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon()
+{
+}
+
+Weapon::Weapon(std::string type)
+{
+	this->type = type;
+}
+
+Weapon::~Weapon()
+{
+}
+
+std::string const &Weapon::getType()
+{
+	return (this->type);
+}
+
+void	Weapon::setType(std::string type)
+{
+	this->type = type;
+}
